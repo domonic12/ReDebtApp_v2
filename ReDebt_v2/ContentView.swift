162528e3762ec83d80e72aa175b31f5a7226a4cc
R.Putri Monica <rajaputrimonica88@gmail.com>
2023-08-21 09:57:18 +0700

    Initial Commit

---
+++ ReDebt_v2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ReDebt_v2
+//
+//  Created by Raja Monica on 21/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReDebt_v2/ReDebt_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  ReDebt_v2App.swift
+//  ReDebt_v2
+//
+//  Created by Raja Monica on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ReDebt_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
